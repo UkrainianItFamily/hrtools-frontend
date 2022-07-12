7bd6df723b83f36ad95878c59a7324a0e090a42f
Anton Titov <108624717+AntonTitovUITF@users.noreply.github.com>
2022-07-12 20:37:09 +0300

    fix: removed strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import { Global } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,14 +10,12 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Global styles={GlobalStyles} />
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </Router>
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <Global styles={GlobalStyles} />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </Provider>,
 );
